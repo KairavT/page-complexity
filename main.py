3d5a5cbb924ef03e46fcc76d89f3ee96b5b18910
Kairav <kairavtupil@gmail.com>
2026-07-10 16:39:17 -0400

    added blurring and edge detection to main.py, added gitignore, README (currently empty), requirements, and the test image

--- main.py
+++ main.py
@@ -4,7 +4,14 @@ import matplotlib.pyplot as plt
 test = cv.imread('images/IMG_8262.jpg')
 test_grey = cv.cvtColor(test, cv.COLOR_BGR2GRAY)
 
-plt.imshow(test)
-plt.show()
-plt.imshow(test_grey, cmap="gray")
+#plt.imshow(test)
+#plt.show()
+#plt.imshow(test_grey, cmap="gray")
+#plt.show()
+
+test_blurred = cv.GaussianBlur(test_grey, (5,5), sigmaX=0)
+#plt.imshow(test_blurred, cmap="gray")
+#plt.show()
+test_canny = cv.Canny(test_blurred, 50, 150)
+plt.imshow(test_canny,cmap="gray")
 plt.show()
